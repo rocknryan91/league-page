b7a7e8aa78472fe5313b3b9d896060487803fc54
rocknryan91 <53800233+rocknryan91@users.noreply.github.com>
2023-05-23 21:44:01 -0500

    Update leagueInfo.js
    Super Dynasty Bros 3 league

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // your league ID
-export const leagueName = "your_league_name"; // your league name
+export const leagueID = "964365449762783232"; // your league ID
+export const leagueName = "Super Dynasty Bros 3"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
